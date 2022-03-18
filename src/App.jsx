e8762031bf014b4cae407fc749f07c112a7fa56c
filipi <filipisee@hotmail.com>
2022-03-18 00:49:08 -0300

    working on board structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import Board from "./components/Board";
 import Keyboard from "./components/Keyboard";
+import { createContext, useState } from 'react';
+import { boardDefault } from './Words';
+
+export const AppContext = createContext();
 
 /* Styles */
 import './App.css'
 
 function App() {
-  
+  const [board, setBoard] = useState(boardDefault);
 
   return (
     <div className="App">
       <nav>
         <h1>Wordle Game</h1>
+      </nav>
+      <AppContext.Provider value={{ board, setBoard }}>
         <Board />
         <Keyboard />
-      </nav>
+      </AppContext.Provider>
     </div>
   )
 }
