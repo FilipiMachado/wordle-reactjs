57ac47b641aeac20c6459b09d36b0ab9d006b1d4
filipi <filipisee@hotmail.com>
2022-03-19 11:03:26 -0300

    added select key value

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
 
-function Key({ keyVal }) {
+function Key({ keyVal, bigKey }) {
+  const { board, setBoard } = useContext(AppContext);
+
+  const selectLetter = () => {
+    const newBoard = [...board]
+    newBoard[0][0] = keyVal
+    setBoard(newBoard)
+  }
+
   return (
-    <div className="key">{ keyVal }</div>
+    <div className="key" id={bigKey && 'big'} onClick={selectLetter}>
+      { keyVal }
+    </div>
   )
 }
 
--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -19,10 +19,11 @@ function Keyboard() {
       })}
       </div>
       <div className="line3">
-        <Key keyVal={'ENTER'}/>
+        <Key keyVal={'ENTER'} bigKey/>
         {keys3.map((key) => {
           return <Key keyVal={key}/>
       })}
+        <Key keyVal={'DELETE'} bigKey/>
       </div>
     </div>
   )
