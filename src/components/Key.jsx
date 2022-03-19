4e6a90ee7e0a97583eb3d011e91a6bf7225781c2
filipi <filipisee@hotmail.com>
2022-03-19 14:52:21 -0300

    adding logic to key component

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -2,21 +2,15 @@ import React, { useContext } from 'react';
 import { AppContext } from '../App';
 
 function Key({ keyVal, bigKey }) {
-  const { board, setBoard, currAttempt, setCurrAttempt } = useContext(AppContext);
+  const { onDelete, onEnter, onSelectLetter } = useContext(AppContext);
 
   const selectLetter = () => {
     if (keyVal === 'ENTER') {
-      if (currAttempt.letterPos !== 5) return;
-      setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 })
+      onEnter();
     } else if (keyVal === 'DELETE') {
-      const newBoard = [...board]
-      newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal
+      onDelete();
     } else {
-      if (currAttempt.letterPos > 4) return;
-      const newBoard = [...board]
-      newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal
-      setBoard(newBoard)
-      setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 })
+      onSelectLetter(keyVal);
     }
   };
 
